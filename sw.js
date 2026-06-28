b8c9a62895e1400811b3519bf524c7bce831d095
akhil4738 <97153496+akhil4738@users.noreply.github.com>
2026-06-28 21:36:43 +0530

    Update cache version to v261

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'akhil-portfolio-v260';
+const CACHE = 'akhil-portfolio-v261';
 const FILES = ['./', './index.html', './manifest.webmanifest'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))); });
